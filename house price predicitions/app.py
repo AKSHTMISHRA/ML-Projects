9c202e3f72563dbcc154fe74042592dae1e65010
Akshat Mishra <akshatnaveenmishra@gmail.com>
2023-09-06 14:25:36 +0530

    version 0.4.2

--- house price predicitions/app.py
+++ house price predicitions/app.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///HOUSE_PRICES.db"
+db=SQLAlchemy(app)
+
+
 
 @app.route("/")
 @app.route("/home")
